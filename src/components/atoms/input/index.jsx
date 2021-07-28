6d7650a6a9b0ea5f31209fa963c1907aed573f5a
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-07-27 22:49:34 -0300

    Update Input atom with clear button and prop

--- src/components/atoms/input/index.jsx
+++ src/components/atoms/input/index.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import IconDelete from '../../../public/icons/icon-delete.svg'
+
 import styles from './input.module.scss'
 
-export const Input = (props) => <input {...props} className={styles.input} />
+export const Input = (props) => (
+  <div className={styles.container}>
+    <input {...props} className={styles.input} />
+
+    {props.value && (
+      <img
+        src={IconDelete}
+        className={styles.iconDelete}
+        onClick={props.clear}
+      />
+    )}
+  </div>
+)
 
 Input.defaultProps = {
-  placeholder: 'Type something...'
+  placeholder: 'Type something...',
+  value: '',
+  clear: null
 }
 
 Input.propTypes = {
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  clear: PropTypes.func
 }
 
 export default Input
--- src/components/atoms/input/input.stories.jsx
+++ src/components/atoms/input/input.stories.jsx
@@ -16,5 +16,23 @@ export const Empty = Template.bind({})
 
 export const WithContent = Template.bind({})
 WithContent.args = {
+  value: 'Something',
+  clear: () => alert('clear')
+}
+
+export const EmptyInDark = Template.bind({})
+EmptyInDark.parameters = {
+  backgrounds: {
+    default: 'dark'
+  }
+}
+
+export const WithContentInDark = Template.bind({})
+WithContentInDark.args = {
   value: 'Something'
+}
+WithContentInDark.parameters = {
+  backgrounds: {
+    default: 'dark'
+  }
 }
