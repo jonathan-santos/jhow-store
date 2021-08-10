f3f19967c576e7fddf269f44e560d600df57f3da
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-08-10 19:49:31 -0300

    Fix productCarousel display

--- src/components/molecules/productCarousel/index.jsx
+++ src/components/molecules/productCarousel/index.jsx
@@ -4,10 +4,29 @@ import PropTypes from 'prop-types'
 import styles from './productCarousel.module.scss'
 
 export const ProductCarousel = ({ items }) => (
-  <div className={styles['product-carousel']}>
+  <div className={styles.container}>
     {items.map((item, i) => (
-      <img key={i} src={item.src} alt={item.alt} />
+      <input
+        key={i}
+        id={`item-${i}`}
+        checked={i === 0}
+        type="radio"
+        name="slider"
+        className={styles[`radio-${i}`]}
+      />
     ))}
+
+    <div className={styles.items}>
+      {items.map((item, i) => (
+        <label
+          key={i}
+          htmlFor={`item-${i}`}
+          className={`${styles.item} ${styles[`item-${i}`]}`}>
+          <img src={item.src} alt={item.alt} />
+          {i}
+        </label>
+      ))}
+    </div>
   </div>
 )
 
