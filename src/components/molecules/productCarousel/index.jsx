658e67bac5ad959670de53caa2fc52649e5afaa6
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-08-11 20:40:23 -0300

    Update productCarousel styles

--- src/components/molecules/productCarousel/index.jsx
+++ src/components/molecules/productCarousel/index.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './productCarousel.module.scss'
 
-export const ProductCarousel = ({ items }) => (
-  <div className={styles.container}>
-    {items.map((item, i) => (
-      <input
-        key={i}
-        id={`item-${i}`}
-        checked={i === 0}
-        type="radio"
-        name="slider"
-        className={styles[`radio-${i}`]}
-      />
-    ))}
+export const ProductCarousel = ({ items }) => {
+  const [selectedItem, setSelectedItem] = useState(0)
 
-    <div className={styles.items}>
+  return (
+    <div className={styles.container}>
       {items.map((item, i) => (
-        <label
+        <input
           key={i}
-          htmlFor={`item-${i}`}
-          className={`${styles.item} ${styles[`item-${i}`]}`}>
-          <img src={item.src} alt={item.alt} />
-          {i}
-        </label>
+          id={`item-${i}`}
+          checked={selectedItem === i}
+          onChange={() => setSelectedItem(i)}
+          type="radio"
+          name="slider"
+          className={styles[`radio-${i}`]}
+        />
       ))}
+
+      <div className={styles.items}>
+        {items.map((item, i) => (
+          <label
+            key={i}
+            htmlFor={`item-${i}`}
+            className={`${styles.item} ${styles[`item-${i}`]}`}>
+            <img src={item.src} alt={item.alt} />
+          </label>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 ProductCarousel.propTypes = {
   items: PropTypes.arrayOf(
