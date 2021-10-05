56bf00c77dfbafc290bca716a056641006329607
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-10-05 07:52:09 -0300

    Create prop color for atom component Title and create global css var color-background-dark

--- src/components/atoms/title/index.jsx
+++ src/components/atoms/title/index.jsx
@@ -5,11 +5,16 @@ import classnames from 'classnames'
 import styles from './title.module.scss'
 
 export const Title = (props) => {
-  const { children, level, className } = props
+  const { children, level, color, className } = props
 
   const TitleElement = `h${level}`
 
-  const classNames = classnames(styles.title, styles[`h${level}`], className)
+  const classNames = classnames(
+    styles.title,
+    styles[`h${level}`],
+    styles[color],
+    className
+  )
 
   return (
     <TitleElement {...props} className={classNames}>
@@ -20,12 +25,14 @@ export const Title = (props) => {
 
 Title.defaultProps = {
   level: '1',
+  color: 'dark',
   className: ''
 }
 
 Title.propTypes = {
   children: PropTypes.node.isRequired,
   level: PropTypes.oneOf(['1', '2', '3', '4', '5', '6']),
+  color: PropTypes.oneOf(['dark', 'light']),
   className: PropTypes.string
 }
 
